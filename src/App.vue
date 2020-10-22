2fedbd06572aba576eed4df652b5cdfc43bb0589
sugai-yuki <ryyeguess4120@gmail.com>
2020-10-22 13:39:18 +0900

    未完

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="zipCode">
+    <button>住所自動入力</button>
+    <p>Address：{{address}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default{
+  data(){
+    return{
+      zipCode: "",
+      address: "",
+    };
+  },
+  async created(){
+     let api = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes?filter=postcode==2790031&apiKey=(apikey)`);
+     this.data = api.data;
+     this.address = this.data.allAddress;
+  }
+};
+
+</script>
+
+<style>
+
+</style>
